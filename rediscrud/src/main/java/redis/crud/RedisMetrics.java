b9f40c3f765d5123eda49c8d5a02b839f2a5591c
Tatiana Karasyova <tatyana.karasyova@macys.com>
2017-04-04 09:07:02 +0200

    assertion added

--- rediscrud/src/main/java/redis/crud/RedisMetrics.java
+++ rediscrud/src/main/java/redis/crud/RedisMetrics.java
@@ -7,6 +7,7 @@ package redis.crud;
     import java.util.Map;
     import static org.junit.Assert.assertEquals;
     import static org.junit.Assert.assertNotNull;
+    import static org.junit.Assert.assertTrue;
 
 public class RedisMetrics {
 
@@ -22,19 +23,26 @@ public class RedisMetrics {
     @Test
     public void info() {
         Jedis jedis = pool.getResource();
-        int allowed_used_cpu_sys = 150;
         String info = jedis.info();
         assertNotNull(info);
         info = jedis.info("CPU");
+        System.out.println(info);
         Map<String, String> convertInfo = convert(info);
         String usedCpu = convertInfo.get("used_cpu_sys");
         //assert the usedCpu is less then a maximum value
+        System.out.println("used_cpu_sys " + usedCpu);
+        Float ucedCpuF = Float.parseFloat(usedCpu);
+        assertTrue(ucedCpuF< 150); 
+
     }
 
     public static Map<String, String> convert(String info) {
-        String[] tokens = info.split("\r\n|:");
+        String[] tokens = info.split("\r\n",2)[1].split("\r\n");
         Map<String, String> map = new HashMap<>();
-        for (int i = 0; i < tokens.length - 1; ) map.put(tokens[i++], tokens[i++]);
+        for (int i = 0; i < tokens.length - 1; i++) {
+            String[] token = tokens[i].split(":");
+            map.put(token[0], token[1]);
+        };
         return map;
     }
 }
